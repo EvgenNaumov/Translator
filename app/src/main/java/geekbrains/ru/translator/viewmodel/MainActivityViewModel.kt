5f2667d1543bd911a0c2f85c03ae4a402ad9c9e8
EvgenNaumov <naumovev@mail.ru>
2023-02-01 16:56:18 +0300

    HW2. Добавил MainViewActivity

---
+++ app/src/main/java/geekbrains/ru/translator/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,53 @@
+package geekbrains.ru.translator.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import geekbrains.ru.translator.model.data.AppState
+import geekbrains.ru.translator.model.datasource.DataSourceLocal
+import geekbrains.ru.translator.model.datasource.DataSourceRemote
+import geekbrains.ru.translator.model.repository.RepositoryImplementation
+import geekbrains.ru.translator.view.main.MainInteractor
+import io.reactivex.observers.DisposableObserver
+import java.util.*
+
+class MainActivityViewModel(
+    private val interactor: MainInteractor = MainInteractor(
+        remoteRepository = RepositoryImplementation(DataSourceRemote()),
+        localRepository = RepositoryImplementation(DataSourceLocal())
+    )
+) : BaseViewModel<AppState>() {
+
+    private var appState: AppState? = null
+
+    override fun getData(word: String, isOnline: Boolean): LiveData<AppState> {
+        compositeDisposable.add(
+            interactor.getData(word,isOnline)
+                .subscribeOn(shedulerProvider.io())
+                .observeOn(shedulerProvider.ui())
+                .doOnSubscribe{liveDataForViewToObserve.value = AppState.Loading(null)}
+                .subscribeWith(getObserver())
+        )
+        return super.getData(word, isOnline)
+    }
+
+    private fun getObserver(): DisposableObserver<AppState> {
+        return object:DisposableObserver<AppState>(){
+            override fun onNext(t: AppState) {
+                appState = t
+                liveDataForViewToObserve.value = t
+            }
+
+            override fun onError(e: Throwable) {
+                liveDataForViewToObserve.value = AppState.Error(e)
+            }
+
+            override fun onComplete() {
+
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
